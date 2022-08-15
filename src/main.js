3a3433362a4b69bf7d94aecab1cb3910ac1972a8
Denis Morev <morev.denis@gmail.com>
2022-08-15 21:16:23 +0300

    создать модуль header-profile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import HeaderProfileView from './view/header-profile-view.js';
 import SortView from './view/sort-view.js';
 import { render } from './render.js';
 
+const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
+render(new HeaderProfileView(), siteHeaderElement);
 render(new SortView(), siteMainElement);
