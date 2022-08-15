3ed8d9db28fd7621a5a9f35cff7a9f3b96d8ffb9
Denis Morev <morev.denis@gmail.com>
2022-08-15 21:27:19 +0300

    создать модуль footer-statistics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+import { render } from './render.js';
+
 import HeaderProfileView from './view/header-profile-view.js';
 import MainNavigationView from './view/main-navigation-view.js';
 import SortView from './view/sort-view.js';
-import { render } from './render.js';
+import FooterStatisticsView from './view/footer-statistics-view.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
 
 render(new HeaderProfileView(), siteHeaderElement);
 render(new MainNavigationView(), siteMainElement);
 render(new SortView(), siteMainElement);
+render(new FooterStatisticsView(), footerStatisticsElement);
