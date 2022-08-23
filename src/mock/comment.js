2d04e2250f4e0c3212cab53465a790d4bb7e873e
Denis Morev <morev.denis@gmail.com>
2022-08-23 21:03:09 +0300

    рандомизировать моковые данные film

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -1,6 +1,6 @@
 export const generateComment = () => ({
   id: '1',
-  author: "Ilya O'Reilly",
+  author: 'Ilya O\'Reilly',
   comment:
     'a film that changed my life, a true masterpiece, post-creditscene was just amazing omg.',
   date: '2019-05-11T16:12:32.554Z',
--- src/utils.js
+++ src/utils.js
@@ -5,4 +5,11 @@ const convertMinutesToHours = (minutesDuration) => {
   return hours < 1 ? `${minutes}m` : `${hours}h ${minutes}m`;
 };
 
-export { convertMinutesToHours };
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export { convertMinutesToHours, getRandomInteger };
