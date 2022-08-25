5022310bf8f28dd5f6cfd1c84fbd004004caeca2
Denis Morev <morev.denis@gmail.com>
2022-08-25 23:58:42 +0300

    написать модель комментариев

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,7 +1,39 @@
 import { generateComment } from '../mock/comment.js';
 
-export default class CommentsModel {
-  comments = Array.from({ length: 5 }, generateComment);
+const getCommentsCount = (films) => films.reduce((count, film) => count + film.comments.length, 0);
 
-  getComments = () => this.comments;
+const generateComments = (films) => {
+  const commentsCount = getCommentsCount(films);
+
+  return Array.from({ length: commentsCount }, (_value, index) => {
+    const commentItem = generateComment();
+
+    return {
+      id: String(index + 1),
+      ...commentItem,
+    };
+  });
+};
+
+export default class CommentsModel {
+  filmsModel = null;
+  allComments = [];
+  filmComments = [];
+
+  constructor(filmsModel) {
+    this.filmsModel = filmsModel;
+    this.generateAllComments();
+  }
+
+  generateAllComments() {
+    this.allComments = generateComments(this.filmsModel.getFilms());
+  }
+
+  getComments = (film) => {
+    this.filmComments = film.comments.map((commentId) =>
+      this.allComments.find((comment) => comment.id === commentId),
+    );
+
+    return this.filmComments;
+  };
 }
