6d3f0e5df1c4a674f9bad3ca932dafc9898e6b6f
Denis Morev <morev.denis@gmail.com>
2022-08-25 23:56:40 +0300

    оставить в объекте фильма только данные из поля filmDetails

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,32 +1,21 @@
-import { getRandomInteger } from '../utils.js';
+import { getRandomInteger, getRandomItem } from '../utils.js';
 
 import { FilmMockData } from '../constants.js';
-import { getRandomItem } from '../utils.js';
 
 export const generateFilm = () => ({
-  id: '0',
-  comments: ['1', '2'],
-  filmInfo: {
-    title: getRandomItem(FilmMockData.TITLES),
-    alternativeTitle: getRandomItem(FilmMockData.TITLES),
-    totalRating: (getRandomInteger(2, 10) - Math.random()).toFixed(1),
-    poster: getRandomItem(FilmMockData.POSTERS),
-    ageRating: getRandomInteger(FilmMockData.AgeRating.MIN, FilmMockData.AgeRating.MAX),
-    director: getRandomItem(FilmMockData.DIRECTORS),
-    writers: getRandomItem(FilmMockData.WRITERS),
-    actors: getRandomItem(FilmMockData.ACTORS),
-    release: {
-      date: getRandomItem(FilmMockData.DATES),
-      releaseCountry: getRandomItem(FilmMockData.COUNTRIES),
-    },
-    runtime: getRandomInteger(FilmMockData.Runtime.MIN, FilmMockData.Runtime.MAX),
-    genre: getRandomItem(FilmMockData.GENRES),
-    description: getRandomItem(FilmMockData.DESCRIPTIONS),
+  title: getRandomItem(FilmMockData.TITLES),
+  alternativeTitle: getRandomItem(FilmMockData.TITLES),
+  totalRating: (getRandomInteger(2, 10) - Math.random()).toFixed(1),
+  poster: getRandomItem(FilmMockData.POSTERS),
+  ageRating: getRandomInteger(FilmMockData.AgeRating.MIN, FilmMockData.AgeRating.MAX),
+  director: getRandomItem(FilmMockData.DIRECTORS),
+  writers: getRandomItem(FilmMockData.WRITERS),
+  actors: getRandomItem(FilmMockData.ACTORS),
+  release: {
+    date: getRandomItem(FilmMockData.DATES),
+    releaseCountry: getRandomItem(FilmMockData.COUNTRIES),
   },
-  userDetails: {
-    watchlist: false,
-    alreadyWatched: true,
-    watchingDate: '2019-04-12T16:12:32.554Z',
-    favorite: false,
-  },
+  runtime: getRandomInteger(FilmMockData.Runtime.MIN, FilmMockData.Runtime.MAX),
+  genre: getRandomItem(FilmMockData.GENRES),
+  description: getRandomItem(FilmMockData.DESCRIPTIONS),
 });
