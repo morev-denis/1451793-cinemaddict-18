f82a1879fba558e48a8f6fc1399f39d260c64c37
Denis Morev <morev.denis@gmail.com>
2022-08-25 23:53:26 +0300

    реализовать случайную генерацию моковых данных комментария

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -1,8 +1,10 @@
+import { CommentMockData } from '../constants.js';
+
+import { getRandomItem } from '../utils.js';
+
 export const generateComment = () => ({
-  id: '1',
-  author: 'Ilya O\'Reilly',
-  comment:
-    'a film that changed my life, a true masterpiece, post-creditscene was just amazing omg.',
-  date: '2019-05-11T16:12:32.554Z',
-  emotion: 'smile',
+  author: getRandomItem(CommentMockData.AUTHORS),
+  comment: getRandomItem(CommentMockData.COMMENTS),
+  date: getRandomItem(CommentMockData.DATES),
+  emotion: getRandomItem(CommentMockData.EMOTIONS),
 });
