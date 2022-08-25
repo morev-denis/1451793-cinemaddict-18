e1a6943874ea1835da529240b2dbc5bf1a79a27b
Denis Morev <morev.denis@gmail.com>
2022-08-25 23:55:25 +0300

    убрать из шаблона заголовок с количеством комментариев

--- src/view/film-details-bottom-container-view.js
+++ src/view/film-details-bottom-container-view.js
@@ -3,7 +3,6 @@ import { createElement } from '../render.js';
 const createFilmDetailsBottomContainerTemplate = () =>
   `<div class="film-details__bottom-container">
     <section class="film-details__comments-wrap">
-      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
     </section>
   </div>`;
 
