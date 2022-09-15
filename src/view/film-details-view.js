44167f639904dd3193e951241c99540ff6f70bff
Denis Morev <morev.denis@gmail.com>
2022-09-15 19:35:41 +0300

    добавить восстановление обработчиков

--- src/view/film-details-view.js
+++ src/view/film-details-view.js
@@ -57,6 +57,14 @@ export default class FilmDetailsView extends AbstractStatefulView {
       .addEventListener('click', this.#favoriteClickHandler);
   };
 
+  _restoreHandlers = () => {
+    this.#setInnerHandlers();
+    this.setCloseButtonClickHandler(this._callback.closeButtonClick);
+    this.setWatchlistClickHandler(this._callback.watchlistClick);
+    this.setAlreadyWatchedClickHandler(this._callback.alreadyWatchedClick);
+    this.setFavoriteClickHandler(this._callback.favoriteClick);
+  };
+
   #favoriteClickHandler = (evt) => {
     evt.preventDefault();
     this._callback.favoriteClick();
@@ -69,7 +77,7 @@ export default class FilmDetailsView extends AbstractStatefulView {
       const input = this.element.querySelector(`#${inputId}`);
       const inputValue = input.value;
 
-      this._state = { ...this._state, selectedEmoji: inputValue };
+      this.updateElement({ selectedEmoji: inputValue });
     }
   };
 
