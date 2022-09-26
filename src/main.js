28cd8010fbf2f44753a6609cb5435b360e28850e
Denis Morev <morev.denis@gmail.com>
2022-09-26 20:35:42 +0300

    добавить модель для фильтра

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import FilmsPresenter from './presenter/films-presenter.js';
 
 import FilmsModel from './model/films-model.js';
 import CommentsModel from './model/comments-model.js';
+import FilterModel from './model/filter-model.js';
 
 const siteMainElement = document.querySelector('.main');
 
 const filmsModel = new FilmsModel();
 const commentsModel = new CommentsModel(filmsModel);
+const filterModel = new FilterModel();
 
 const filmsPresenter = new FilmsPresenter(siteMainElement, filmsModel, commentsModel);
 
