b955d7da0087a6fc79cd21b93ed6b4ba2548cfc0
Denis Morev <morev.denis@gmail.com>
2022-09-28 22:34:20 +0300

    реализовать паттерн Адаптер

--- src/films-api-service.js
+++ src/films-api-service.js
@@ -6,4 +6,26 @@ export default class FilmsApiService extends ApiService {
   get films() {
     return this._load({ url: 'movies' }).then(ApiService.parseResponse);
   }
+
+  #adaptToServer = (film) => {
+    const adaptedFilm = {
+      ...film,
+      'film_info': {
+        ...film.filmInfo,
+        'alternative_title': film.filmInfo.alternativeTitle,
+        'total_rating': film.filmInfo.totalRating,
+        'age_rating': film.filmInfo.ageRating,
+      },
+      'user_details': {
+        ...film.userDetails,
+        'already_watched': film.userDetails.alreadyWatched,
+        'watching_date': film.userDetails.watchingDate,
+      },
+    };
+
+    delete adaptedFilm.filmInfo;
+    delete adaptedFilm.userDetails;
+
+    return adaptedFilm;
+  };
 }
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -11,10 +11,32 @@ export default class FilmsModel extends Observable {
     super();
     this.#filmsApiService = filmsApiService;
 
-    filmsApiService.films.then((films) => {
-      console.log(films);
+    this.#filmsApiService.films.then((films) => {
+      console.log(films.map(this.#adaptToClient));
     });
   }
+
+  #adaptToClient = (film) => {
+    const adaptedFilm = {
+      ...film,
+      filmInfo: {
+        ...film['film_info'],
+        alternativeTitle: film['film_info']['alternative_title'],
+        totalRating: film['film_info']['total_rating'],
+        ageRating: film['film_info']['age_rating'],
+      },
+      userDetails: {
+        ...film['user_details'],
+        alreadyWatched: film['user_details']['already_watched'],
+        watchingDate: film['user_details']['watching_date'],
+      },
+    };
+
+    delete adaptedFilm['film_info'];
+    delete adaptedFilm['user_details'];
+
+    return adaptedFilm;
+  };
 
   #generateFilms = () => {
     const films = Array.from({ length: FILM_COUNT }, generateFilm);
