08d1d1b7b3eda01f63135f9caac8b5399c6246c9
Denis Morev <morev.denis@gmail.com>
2022-09-28 18:58:15 +0300

    реализовать безопасный ввод

--- src/view/film-details-template.js
+++ src/view/film-details-template.js
@@ -1,3 +1,4 @@
+import he from 'he';
 import {
   convertMinutesToHours,
   formatISOStringToDate,
@@ -14,7 +15,7 @@ const createFilmDetailsCommentTemplate = (commentObj) => {
       <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-smile">
       </span>
       <div>
-        <p class="film-details__comment-text">${comment}</p>
+        <p class="film-details__comment-text">${he.encode(comment)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${formatISOStringToRelativeTime(date)}</span>
