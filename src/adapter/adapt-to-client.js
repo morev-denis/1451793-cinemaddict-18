0d7369f8afa7c291b708b632f26ec45a77c79217
Denis Morev <morev.denis@gmail.com>
2022-10-03 10:27:34 +0300

    исправить адаптер adapt-to-client

--- src/adapter/adapt-to-client.js
+++ src/adapter/adapt-to-client.js
@@ -6,6 +6,10 @@ export const adaptToClient = (film) => {
       alternativeTitle: film['film_info']['alternative_title'],
       totalRating: film['film_info']['total_rating'],
       ageRating: film['film_info']['age_rating'],
+      release: {
+        ...film['film_info'].release,
+        releaseCountry: film['film_info'].release['release_country'],
+      },
     },
     userDetails: {
       ...film['user_details'],
@@ -15,7 +19,13 @@ export const adaptToClient = (film) => {
   };
 
   delete adaptedFilm['film_info'];
+  delete adaptedFilm.filmInfo['alternative_title'];
+  delete adaptedFilm.filmInfo['total_rating'];
+  delete adaptedFilm.filmInfo['age_rating'];
+  delete adaptedFilm.filmInfo.release['release_country'];
   delete adaptedFilm['user_details'];
+  delete adaptedFilm.userDetails['already_watched'];
+  delete adaptedFilm.userDetails['watching_date'];
 
   return adaptedFilm;
 };
